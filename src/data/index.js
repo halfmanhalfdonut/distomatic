6bec21dd730e3569835a2cab27cc74e4209d31d3
Nick Jurista <nick@inexact-scientist.com>
2021-03-21 20:47:12 -0400

    Adding major league rugby

--- src/data/index.js
+++ src/data/index.js
@@ -2,8 +2,9 @@ import NFL from './nfl.js';
 import NHL from './nhl.js';
 import NBA from './nba.js';
 import MLB from './mlb.js';
+import MLR from './mlr.js';
 import MLS from './mls.js';
 //import AAF from './aaf.js';
 import XFL from './xfl.js';
 
-export { NFL, NHL, NBA, MLB, MLS, XFL };
+export { NFL, NHL, NBA, MLB, MLR, MLS, XFL };
